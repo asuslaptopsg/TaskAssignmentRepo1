3beb0464b49d304f78df139192ca6a56ab551a63
Bob <asuslaptopsg@gmail.com>
2022-12-11 20:54:06 +0800

    applicationContext.xml setup

--- TaskAutomation/src/main/java/com/singapore/bpm/taskautomation/primary/TaskAssignment.java
+++ TaskAutomation/src/main/java/com/singapore/bpm/taskautomation/primary/TaskAssignment.java
@@ -1,10 +1,30 @@
 package com.singapore.bpm.taskautomation.primary;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.singapore.bpm.taskautomation.DTO.User;
+
 //Adding comments direct on server
 public class TaskAssignment {
      // new local changes to push to remote
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		new TaskAssignment().TaskAssignmentProcess();
+	}
+	
+	
+	public void TaskAssignmentProcess(){
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		User user = (User)context.getBean("userBeanID");
+		
+		
+		System.out.println("--usrname--"+user.getUsername());
+		System.out.println("--password--"+user.getPassword());
 	}
 
 }
