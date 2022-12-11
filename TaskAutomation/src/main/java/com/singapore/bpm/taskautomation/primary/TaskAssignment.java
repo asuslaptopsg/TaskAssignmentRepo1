cc58dd387301c8ff0328d4917ed2bdd55413e11b
Bob <asuslaptopsg@gmail.com>
2022-12-11 19:47:24 +0800

    locally adding comments for method

--- TaskAutomation/src/main/java/com/singapore/bpm/taskautomation/primary/TaskAssignment.java
+++ TaskAutomation/src/main/java/com/singapore/bpm/taskautomation/primary/TaskAssignment.java
@@ -1,7 +1,7 @@
 package com.singapore.bpm.taskautomation.primary;
 
 public class TaskAssignment {
-
+      // adding comments for method on eclipse locally
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
