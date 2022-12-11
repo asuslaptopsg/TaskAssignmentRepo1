ec6e40a16848bfa038ba0b9fe1da3b0f6e79a145
Bob <asuslaptopsg@gmail.com>
2022-12-11 19:56:58 +0800

    locall commit

--- TaskAutomation/src/main/java/com/singapore/bpm/taskautomation/primary/TaskAssignment.java
+++ TaskAutomation/src/main/java/com/singapore/bpm/taskautomation/primary/TaskAssignment.java
@@ -1,7 +1,7 @@
 package com.singapore.bpm.taskautomation.primary;
 
 public class TaskAssignment {
-
+     // new local changes to push to remote
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
